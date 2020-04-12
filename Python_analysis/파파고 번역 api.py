b5b8af08045b2fcc535a3dfcde11c00f0e883baf
YoungWoon Jo <bbiyongel@gmail.com>
2020-04-12 18:17:18 +0900

    음성합성

--- Python_analysis/파파고 번역 api.py
+++ Python_analysis/파파고 번역 api.py
@@ -3,3 +3,24 @@
 
 https://wikidocs.net/35983
 
+
+아이유 음성합성 
+
+https://blog.crux.cx/iu-siri-3/
+
+파파고 음성합성
+https://wikidocs.net/15213
+
+자모 
+
+https://juheo.tistory.com/6
+
+타코트론
+https://github.com/sokcuri/multi-speaker-tacotron-tensorflow/blob/master/README_ko.md
+
+
+https://engineering.linecorp.com/ko/blog/voice-waveform-arbitrary-signal-to-noise-ratio-python/
+https://github.com/Sato-Kunihiko/audio-SNR
+
+git init 
+git pull https://github.com/carpedm20/multi-Speaker-tacotron-tensorflow
