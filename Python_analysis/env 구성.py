9b3a603da6bdf9e23529219d602768fccdf20fbc
YoungWoon Jo <bbiyongel@gmail.com>
2020-04-25 04:58:50 +0900

    timeseries

---
+++ Python_analysis/env 구성.py
@@ -0,0 +1,13 @@
+env 구성
+
+주피터 > new > 터미널 
+
+가상환경만들기 
+conda create --name timeseries --file requirements.txt 
+
+activate timeseries (win)
+
+source activate timeseries (mac)
+
+(e) timeseries > conda install jupyter
+
