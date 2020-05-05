269a1d1c042d0eef9e48b5183c4dda281a886fe8
YoungWoon Jo <bbiyongel@gmail.com>
2020-05-05 13:47:56 +0900

    books

--- Python_analysis/kmeans elbow.py
+++ Python_analysis/kmeans elbow.py
@@ -26,3 +26,6 @@ https://medium.com/@nsh235482/k-means-clustering-6ab85a2a32ad
 # 실루엣, t-sne
 https://mkjjo.github.io/finance/2019/01/09/kmeans_corp.html
 http://blog.naver.com/PostView.nhn?blogId=samsjang&logNo=221017639342&categoryNo=0&parentCategoryNo=49&viewDate=&currentPage=1&postListTopCurrentPage=1&from=search
+
+#파이썬 중급자 책
+https://winterj.me/python-books-for-intermediate/
